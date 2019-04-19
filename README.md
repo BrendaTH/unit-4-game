ad0bae96b019ae4406834e80ef3f36ccf21de80e
generic Mac <genericmac@generics-MacBook-Pro.local>
2019-04-19 16:58:43 -0400

    final changes for unit 4 hw assignment

---
+++ README.md
@@ -0,0 +1,8 @@
+Crystal Collector Game
+
+This is a game that dynamically updates the HTML page with the jQuery library.
+It is a Full Stack Web Development unit 4 homework assignment. Unit 4 is all about intro to jQuery. This homework should demonstrate some basic concepts of manipulating the DOM with jQuery.
+
+To get started with the game go [here:](https://brendath.github.io/unit-4-game)
+Find [my portfolio and more about me here:](https://brendath.github.io/Responsive-Portfolio). There you should be able to also see some of my other work.
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,7 @@
     // OBJECTS
     var myGame ={
         totalScore: 0,
-        scoreToBeat: 0, // do we need to keep this? just check the dom?
+        scoreToBeat: 0,
         wins: 0,
         losses: 0,
         initGame: function() {
@@ -21,12 +21,13 @@
             
         },
 
-        // generate random number between 1-99 for scoreToBeat
+        // generate random number between 19-120 for scoreToBeat
         // display scoreToBeat for user to see
         genScoreToBeat: function() {
             this.scoreToBeat = getRndInteger(19, 121);
             $(".score-to-beat").text(this.scoreToBeat);
         },
+
         // generate 4 random numbers between 1 - 12, 1 for each crystal
         // write the value attribute with the random number for each crystal
         genCrystalNumbers: function() {
@@ -36,6 +37,14 @@
             });
         },
 
+        // refreshUserLoses: 
+        // called to:
+        //    update the users losses game variable
+        //    to display an alert if the user lost
+        //    to update the display with the number of loses
+        // a boolean is passed in - didUserLose
+        //     it indicates if user has actually lost a game (true)
+        //     or if we are only updating the display (false) on game init
         refreshUserLoses: function(didUserLose) {
             if (didUserLose) {
                 this.losses++;
@@ -43,6 +52,15 @@
             }
             $(".number-of-losses").text("Losses: " + this.losses);
         },
+
+        // refreshUserWins:
+        // called to:
+        //     update users wins game variable
+        //     to display an alert if the user won
+        //     to update the display with the number of wins
+        // a boolean is passed in - didUserWin
+        //     it indicates if user has actually won a game (true)
+        //     or if we are only updating the display (false) on game init
         refreshUserWins: function(didUserWin) {
             if (didUserWin) {
                 this.wins++;
@@ -50,6 +68,7 @@
             }
             $(".number-of-wins").text("Wins: " + this.wins);
         },
+        
         userContinues:function() {
             // nothing to do yet
             
@@ -66,9 +85,9 @@
     $(document).ready(function() {
         myGame.initGame();
         // when the user clicks on the crystal add the value to the users score
-        // if score is greater that score-to-reach user losses
+        // if score is greater than score-to-reach user losses
         // if score is equal to score-to-reach user wins
-        // if score is less than score-to-reach keep goingf
+        // if score is less than score-to-reach keep going
         $(".crystal-image").on("click", function() {
             myGame.totalScore += parseInt($(this).attr("value"));
             $(".total-score").text(myGame.totalScore);
@@ -83,7 +102,5 @@
             } else {
                 myGame.userContinues();
             }
-
         });
-
     });
